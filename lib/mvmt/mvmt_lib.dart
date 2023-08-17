6b6494d8d14c0a2f08beb6aca6ff8f8f14846476
Okunamutata <tokunna@gmail.com>
2023-08-17 12:07:28 -0700

    delete mvmt_history

--- lib/mvmt/mvmt_lib.dart
+++ lib/mvmt/mvmt_lib.dart
@@ -1,12 +1,9 @@
 library fit_stitch_mvmt;
 
-import 'dart:convert';
-import 'dart:developer';
 
 import 'package:fs_common/extensions.dart';
 import 'package:fs_common/utilities.dart';
 
-part 'mvmt_history.dart';
 
 part 'movement_model.dart';
 
